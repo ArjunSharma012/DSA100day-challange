2aa12faf1970e46735be679d6063528b241af181
ArjunSharma012 <arjunsharma20612@gmail.com>
2026-04-19 18:18:31 +0530

    Add files via upload

---
+++ day78.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int n, m;
+
+int minKey(int key[], bool mstSet[]) {
+    int min = INT_MAX, min_index = -1;
+
+    for (int v = 1; v <= n; v++) {
+        if (!mstSet[v] && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+int primMST() {
+    int key[MAX];
+    bool mstSet[MAX];
+    int totalWeight = 0;
+
+    for (int i = 1; i <= n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
+    }
+
+    key[1] = 0;
+
+    for (int count = 1; count <= n; count++) {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+        totalWeight += key[u];
+
+        for (int v = 1; v <= n; v++) {
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    return totalWeight;
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            graph[i][j] = 0;
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+        graph[v][u] = w;
+    }
+
+    printf("%d\n", primMST());
+
+    return 0;
+}
